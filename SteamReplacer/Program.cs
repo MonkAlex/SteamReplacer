d3e74c087e66f1cdc88c183908cbe23fddd12be0
MonkAlex <3amepob@gmail.com>
2015-08-21 21:11:05 +0400

    Simple working app to run steam "game".

---
+++ SteamReplacer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Steamworks;
+
+namespace SteamReplacer
+{
+  class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
+    {
+      var appId = long.Parse("1200");
+      Environment.SetEnvironmentVariable("SteamAppId", appId.ToString());
+
+      if (!SteamAPI.Init())
+      {
+        return;
+      }
+    }
+  }
+}
