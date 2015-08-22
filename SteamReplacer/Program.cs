5adf369ae49541e73020feec240355bb496ad343
MonkAlex <3amepob@gmail.com>
2015-08-22 07:04:15 +0300

    Run game and close.

--- SteamReplacer/Program.cs
+++ SteamReplacer/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Steamworks;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
 
 namespace SteamReplacer
 {
@@ -8,13 +12,35 @@ namespace SteamReplacer
     [STAThread]
     static void Main(string[] args)
     {
-      var appId = long.Parse("1200");
-      Environment.SetEnvironmentVariable("SteamAppId", appId.ToString());
+      var game = new Game(args);
+      Environment.SetEnvironmentVariable("SteamAppId", game.AppId.ToString());
 
-      if (!SteamAPI.Init())
-      {
-        return;
-      }
+      if (SteamAPI.Init())
+        game.Run();
+    }
+
+  }
+
+  internal class Game
+  {
+    internal long AppId { get; set; }
+
+    internal FileInfo FilePath { get; set; }
+
+    internal bool Exists { get { return this.FilePath.Exists; } }
+
+    internal DirectoryInfo WorkingDirectory { get { return this.FilePath.Directory; } }
+
+    internal void Run()
+    {
+      var processInfo = new ProcessStartInfo() { FileName = this.FilePath.FullName, WorkingDirectory = WorkingDirectory.FullName };
+      Process.Start(processInfo);
+    }
+
+    internal Game(string[] args)
+    {
+      this.AppId = long.Parse(args[0]);
+      this.FilePath = new FileInfo(args[1]);
     }
   }
 }
